1d04fe626267f08b2d34524e861248a3c6fe03d1
Kamalpreet kaur <kkaur_be21@thapar.edu>
2023-06-25 21:08:05 +0530

    Website completed

---
+++ script.js
@@ -0,0 +1,48 @@
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header .navbar a');
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    Array.from(section).forEach(sec => {
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            Array.from(navlinks).forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*="' + id + '"]').classList.add('active');
+            });
+        }
+    });
+};
+
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+    
+}
+
+function loader()
+{
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+    setInterval(loader,3000);
+}
+
+window.onload = fadeOut;
